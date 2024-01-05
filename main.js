3df5a75588d91599ad0b674e718b434c415372c2
Vash4772 <155339171+Vash4772@users.noreply.github.com>
2024-01-05 19:21:13 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+var tl = gsap.timeline({
+    repeat: -1,
+});
+
+tl
+.to(".imagecontainer",{
+    ease: Expo.easeInOut,
+    width: "100%",
+    stagger: 2,
+}, 'a')
+tl.to(".text h1",{
+    ease: Expo.easeInOut,
+    stagger: 2,
+    top: 0
+}, 'a')
+tl.to(".text h1",{
+    delay: 2,
+    ease: Expo.easeInOut,
+    stagger: 2,
+    top: "-100%"
+}, 'a')
